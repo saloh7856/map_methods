bc4d41de8532cb397ab22586b401ddbe733a0262
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-28 11:19:45 +0500

    05

--- bin/map_methods05.dart
+++ bin/map_methods05.dart
@@ -4,10 +4,13 @@ the key in it is last name,
 change it to surname
 */
 
-void main() {}
-
-Map func(Map data) {
+Map func(Map <String,String>data) {
   //your code here
-
+  data.remove('last_name');
+  data['surname']='kerry';
   return data;
+}
+
+void main(){
+  print(func({'login' : 'John', 'last_name':'Kerry'}));
 }
