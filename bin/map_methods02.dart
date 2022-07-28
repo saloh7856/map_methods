b0f3c7301e4814c1baf3b19e56ca3e8464d11480
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-28 09:52:36 +0500

    02

--- bin/map_methods02.dart
+++ bin/map_methods02.dart
@@ -2,10 +2,13 @@
 Car details will be given to you in map view, change the car price for it
 */
 
-void main() {}
+void main(){
+  print(func({'salom':'hello','lion':100},700));
+}
 
 Map func(Map data, int cost) {
   //your code here
+  data['lion']=cost;
 
   return data;
 }
